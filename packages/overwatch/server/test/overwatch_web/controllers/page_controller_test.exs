e3f39bd11d51887f15f24fb50ee1b430475161db
Robert DeRose <RobertDeRose@users.noreply.github.com>
2026-02-01 12:50:10 -0500

    chore(repo): run 'mix phx.new' to create Nixstasis scaffolding

---
+++ packages/overwatch/server/test/overwatch_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule NixstasisWeb.PageControllerTest do
+  use NixstasisWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
