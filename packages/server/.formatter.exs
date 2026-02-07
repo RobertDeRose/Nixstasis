b370e6b192a07b63e8a57780f808843bb13b465d
Robert DeRose <RobertDeRose@users.noreply.github.com>
2026-02-07 00:19:41 -0500

    feat(server): install Ash Framework

--- packages/server/.formatter.exs
+++ packages/server/.formatter.exs
@@ -1,7 +1,18 @@
 [
-  import_deps: [:ecto, :ecto_sql, :phoenix],
+  import_deps: [
+    :ash_state_machine,
+    :ash_admin,
+    :ash_postgres,
+    :ash_json_api,
+    :ash_phoenix,
+    :ash,
+    :reactor,
+    :ecto,
+    :ecto_sql,
+    :phoenix
+  ],
   line_length: 120,
   subdirectories: ["priv/*/migrations"],
-  plugins: [Phoenix.LiveView.HTMLFormatter],
+  plugins: [Spark.Formatter, Phoenix.LiveView.HTMLFormatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
 ]
