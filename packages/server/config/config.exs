4c746e24dcc379071fcfd758c907615e622b6633
Robert DeRose <RobertDeRose@users.noreply.github.com>
2026-02-07 00:19:41 -0500

    feat(server): install Ash Framework

--- packages/server/config/config.exs
+++ packages/server/config/config.exs
@@ -7,6 +7,49 @@
 # General application configuration
 import Config
 
+config :mime, extensions: %{"json" => "application/vnd.api+json"}, types: %{"application/vnd.api+json" => ["json"]}
+config :ash_json_api, show_public_calculations_when_loaded?: false, authorize_update_destroy_with_error?: true
+
+config :ash,
+  allow_forbidden_field_for_relationships_by_default?: true,
+  include_embedded_source_by_default?: false,
+  show_keysets_for_all_actions?: false,
+  default_page_type: :keyset,
+  policies: [no_filter_static_forbidden_reads?: false],
+  keep_read_action_loads_when_loading?: false,
+  default_actions_require_atomic?: true,
+  read_action_after_action_hooks_in_order?: true,
+  bulk_actions_default_to_errors?: true,
+  transaction_rollback_on_error?: true,
+  known_types: [AshPostgres.Timestamptz, AshPostgres.TimestamptzUsec]
+
+config :spark,
+  formatter: [
+    remove_parens?: true,
+    "Ash.Resource": [
+      section_order: [
+        :admin,
+        :postgres,
+        :json_api,
+        :resource,
+        :code_interface,
+        :actions,
+        :policies,
+        :pub_sub,
+        :preparations,
+        :changes,
+        :validations,
+        :multitenancy,
+        :attributes,
+        :relationships,
+        :calculations,
+        :aggregates,
+        :identities
+      ]
+    ],
+    "Ash.Domain": [section_order: [:admin, :json_api, :resources, :policies, :authorization, :domain, :execution]]
+  ]
+
 config :nixstasis,
   ecto_repos: [Nixstasis.Repo],
   generators: [timestamp_type: :utc_datetime]
